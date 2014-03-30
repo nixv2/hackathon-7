841dc7ea4fd0d97dca3442f39f51176fe366be45
Rafael <rafavg77@gmail.com>
2014-03-30 03:17:59 -0600

    ultimo cochinero

--- app/controller/Producto.js
+++ app/controller/Producto.js
@@ -9,7 +9,41 @@ Ext.define('PriceCom.controller.Producto', {
             }
         }
     },
-    guardarProducto: function(){
-        console.log('save');
+
+    guardarProducto: function(button){
+        console.log(button.up('formpanel').getValues());
+        var record = button.up('formpanel').getValues();
+        var form = button.up('formpanel');
+        /*form.submit({
+            params : {
+                producto : record
+            },
+            url : 'http://10.20.218.103:8080/pricecom/producto/save',
+            method : 'POST',
+            success: function() { 
+                Ext.Msg.alert("Alta de Producto."); 
+            }, 
+            failure: function() { 
+                Ext.Msg.alert("Ocurrio un error, Intente de nuevo en un momento.");
+            }
+        });*/
+        Ext.Ajax.request({
+            type: 'POST',
+            contentType:'application/json',
+            dataType: 'jsonp',
+            params: {
+                producto : record
+            },
+            url : 'http://10.20.218.103:8080/pricecom/producto/save',
+            success: function(response, opts) {
+                console.log('server-side success with status code ' + response.status);
+            },
+            failure: function(response, opts) {
+                console.log('server-side failure with status code ' + response.status);
+            },
+            error: function(msg){
+            alert(msg);
+            }
+        });
     }
 });
