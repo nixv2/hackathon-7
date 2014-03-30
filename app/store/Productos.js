d9ae290b34f00bd5d3983fba4383c4ec61508b33
Felipe Villa <felipe.villa@bleext.com>
2014-03-30 00:28:23 -0600

    cargar data del servidor de evi

--- app/store/Productos.js
+++ app/store/Productos.js
@@ -5,9 +5,18 @@ Ext.define('PriceCom.store.Productos',{
     config :{
         model: 'PriceCom.model.Producto',
         autoLoad: true,
-        data: [
-            {id:'1',producto:'papel higienico', marca:'charmin',img:'imgs/avatar.png',precio:'100.4',tienda_locacion:'25.6206579,-100.2793085,15z',tienda_nombre:'walmart',likes:'3',tags:[{name:'papel de banio'},{name:'chiarmin'}]},
-            {id:'2',producto:'Pasta', marca:'Maria',img:'imgs/avatar.png',precio:'10.4',tienda_locacion:'25.6206579,-100.2793085,15z',tienda_nombre:'walmart',likes:'1',tags:[{name:'pasta'},{name:'maria'},{name:'espageti'}]}
-        ]
+        proxy: {
+        type: 'jsonp',
+        url : 'http://10.20.218.103:8080/pricecom/producto/index',
+        reader: {
+            type: 'json',
+            rootProperty: ''
+        }
+    }
+
+        // data: [
+        //     {id:'1',producto:'papel higienico', marca:'charmin',img:'imgs/avatar.png',precio:'100.4',tienda_locacion:'25.6206579,-100.2793085,15z',tienda_nombre:'walmart',likes:'3',tags:[{name:'papel de banio'},{name:'chiarmin'}]},
+        //     {id:'2',producto:'Pasta', marca:'Maria',img:'imgs/avatar.png',precio:'10.4',tienda_locacion:'25.6206579,-100.2793085,15z',tienda_nombre:'walmart',likes:'1',tags:[{name:'pasta'},{name:'maria'},{name:'espageti'}]}
+        // ]
     }
 });
