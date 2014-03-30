5d61816ce09eb06e1d2ed2df4ea201249a4f993f
Felipe Villa <felipe.villa@bleext.com>
2014-03-29 23:31:13 -0600

    add searchbar

--- app/view/ProductosList.js
+++ app/view/ProductosList.js
@@ -12,8 +12,32 @@ Ext.define('PriceCom.view.ProductosList', {
                         xtype: 'button',
                         text: 'add',
                         action : 'add'
+                    },
+                    {
+                        xtype: 'spacer'
+                    },
+                    {
+                        xtype : 'button',
+                        iconCls :'search',
+                        action : 'search'
                     }
                 ]
+            },
+            {
+                xtype   : 'toolbar',
+                docked  : 'top',
+                itemId  : 'searchbar',
+                items   : [{
+                    xtype   : 'searchfield',
+                    flex    : 1,
+                    height  : 27,
+                    placeHolder : 'Busque por palabras o número'
+                },{
+                    xtype   : 'button',
+                    text    : 'Buscar',
+                    action  : 'buscarProducto',
+                    hidden  : true
+                }]
             }
         ],
         store: {
